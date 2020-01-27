4fc25ac6d75ec5f778f138b89f149709a1e972b7
HSNA243 <49468994+HSNA243@users.noreply.github.com>
2020-01-27 20:15:33 +0530

    Add files via upload

---
+++ DFS.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+d = defaultdict(list)
+
+def addedge(x,y):
+    d[x].append(y)
+
+def dfs(d):
+    v = len(d)
+    visits = [False]*v
+    for i in range(v):
+        if visits[i] == False:
+            dfsRec(i,visits)
+
+def dfsRec(i,visits):
+    visits[i] = True
+    print(i,"is visited")
+    for j in d[i]:
+        if visits[j] == False:
+            dfsRec(j,visits)
+
+
+addedge(0, 1) 
+addedge(0, 2) 
+addedge(1, 2) 
+addedge(2, 0) 
+addedge(2, 3) 
+addedge(3, 3) 
+
+dfs(d)
