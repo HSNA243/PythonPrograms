91b4a9c7b940f29f0764703a3768b22338d97abe
HSNA243 <49468994+HSNA243@users.noreply.github.com>
2020-02-10 22:32:23 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,24 +1,26 @@
-
 import math
 
 
-def bs(v , x):
-    m = math.ceil(len(v)/2) - 1
-    if (len(v) == 1):
-        if (v[0] == x):
-            return "Present"
-        else:
-            return "Absent"
-    else:
-        if (x == v[m]):
-            return "Present"
-        elif (x < v[m]):
-            return bs(v[0:m+1],x)
+def bs(v, l, g,  x):
+    m = (l + g) // 2
+    if l > len(v)-1:
+        return -1
+    if g >= 0:
+        if x == v[m]:
+            return m 
+        elif x < v[m]:
+            return bs(v,l,m-1,x)
         else :
-            return bs(v[m+1:],x)
+            return bs(v,m+1,g,x)
+
+
 
 print("Enter the array : ")
 a = [int(i) for i in input().split()]
 print("Enter the number : ")
 x = int(input())
-print(bs(a,x))
+ans = bs(a,0,len(a)-1,x)
+if ans == -1:
+    print("It is not present")
+else:
+    print("It is present at index",ans)
